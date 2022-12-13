1aa2e1495201eb06554dc038120af8b7b069501b
alexsincai <alex.sincai@yahoo.co.uk>
2022-12-13 14:56:38 +0200

    changes

--- apiroutes.js
+++ apiroutes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
-export default Router()
-    .get("/", (req, res) => res.render("index"))
+export default Router() //  export the whole module
+    .get("/", (req, res) => res.render("index")) //  render the form on get
     .post("/", (req, res) => {
-        const { nume, prenume } = req.body;
+        const { nume, prenume } = req.body; //  extract the nume and prenume variables that were submitted
 
         if (req.is("json")) {
             //  if the request is JSON, return JSON
-            res.json({ message: `Am primit numele "${nume}" si prenumele "${prenume}"`, });
+            res.json({
+                message: `Am primit numele "${nume}" si prenumele "${prenume}"`,
+            });
         } else {
             //  otherwise render a file
             res.render("result", { n: nume, p: prenume });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express, { json, urlencoded, static as estatic } from "express";
 import apiRouter from "./apiroutes.js";
 
-express()
-    .use(json())
-    .use(urlencoded({ extended: true }))
-    .set("view engine", "pug")
-    .set("views", "./views")
-    .use(estatic("public"))
-    .get("/", (req, res) => res.render("index"))
-    .use("/api", apiRouter)
-    .listen(9000, () => console.log("http://localhost:9000/"));
+express() //  get the app going
+    .use(json()) //  process JSON data
+    .use(urlencoded({ extended: true })) //  process forms
+    .set("view engine", "pug") //  set the pug view engine
+    .set("views", "./views") //  set the templates folder
+    .use(estatic("public")) //  set the static folder
+    .get("/", (req, res) => res.render("index")) //  render an index page
+    .use("/api", apiRouter) //  render all api pages
+    .listen(9000, () => console.log("http://localhost:9000/")); //  start the app
